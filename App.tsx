c36f385553844ffd33458504395701d2b04a8c81
ManupaDev <manupadevsuite@gmail.com>
2023-02-05 09:56:32 +0530

    init base app

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>This is pretty cool</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fffff2',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    color:'#ff0000',
+    fontSize:32
+  }
 });
