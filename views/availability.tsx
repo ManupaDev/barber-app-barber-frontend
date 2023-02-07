c84da4767cba65b635bccd04c8633823c5ddafbf
ManupaDev <manupadevsuite@gmail.com>
2023-02-07 16:00:06 +0530

    feat:lifted view availability state up

--- views/availability.tsx
+++ views/availability.tsx
@@ -3,9 +3,27 @@ import Header from "../components/header";
 import { Entypo } from "@expo/vector-icons";
 import Card from "../components/card";
 import DateTimeView from "../components/date-time-view";
+import { useState } from "react";
 
 
 function Availability() {
+
+  const [date, setDate] = useState(new Date());
+  const [show, setShow] = useState(false);
+
+  const handleDateChange = (e: any, selectedDate: Date) => {
+    if (e.type === "set") {
+      setDate(selectedDate);
+      setShow(false);
+    } else if (e.type === "dismissed") {
+      setShow(false);
+    }
+  };
+
+  const toggleShow = () => {
+    setShow(true);
+  };
+
   return (
     <View className="w-full border-red-500">
       <Header />
@@ -16,8 +34,9 @@ function Availability() {
 
         <View className=" border-black flex flex-col mt-4">
           <Card preview={false}/>
-          <DateTimeView />
+          <DateTimeView date={date} show={show} handleDateChange={handleDateChange} toggleShow={toggleShow}/>
         </View>
+
       </View>
     </View>
   );
