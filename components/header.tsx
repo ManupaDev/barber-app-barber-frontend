06d235f68906a52ada457b745d8b62cac9c2cd59
ManupaDev <manupadevsuite@gmail.com>
2023-02-11 21:22:37 +0530

    feat:fixed time rendering in the header

--- components/header.tsx
+++ components/header.tsx
@@ -21,7 +21,7 @@ function Header() {
     <View className="px-4 py-4 border border-black rounded-lg">
       <Text className="text-base font-semibold">Welcome, Mr. Ruwan</Text>
       <View className="mt-2">
-        <Text className="text-3xl font-semibold">{date.toLocaleTimeString().slice(0,5)}</Text>
+        <Text className="text-3xl font-semibold">{date.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: false })}</Text>
         <Text className="text-xl font-semibold">{date.toDateString()}</Text>
       </View>
     </View>
